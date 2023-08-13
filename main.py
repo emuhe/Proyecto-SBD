877090753419f05a8dd6558cd4032507e3e3ed18
emuhe <140031745+emuhe@users.noreply.github.com>
2023-08-13 16:35:13 -0500

    arreglado cosas menores

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class MainMenu:
         # setting title
         root.title("BlaBlaCar")
         # setting window size
-        width = 498
+        width = 500
         height = 500
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
@@ -21,7 +21,7 @@ class MainMenu:
         GLabel_551["fg"] = "#333333"
         GLabel_551["justify"] = "center"
         GLabel_551["text"] = "Bienvenido a BlaBlaCar!"
-        GLabel_551.place(x=50, y=40, width=373, height=47)
+        GLabel_551.place(x=70, y=40, width=373, height=47)
 
         GButton_760 = tk.Button(root)
         GButton_760["bg"] = "#f0f0f0"
@@ -30,7 +30,8 @@ class MainMenu:
         GButton_760["fg"] = "#000000"
         GButton_760["justify"] = "center"
         GButton_760["text"] = "Ingresar"
-        GButton_760.place(x=160, y=260, width=120, height=30)
+        GButton_760["relief"] = "groove"
+        GButton_760.place(x=190, y=250, width=120, height=30)
         GButton_760["command"] = self.GButton_760_command
 
         GLabel_238 = tk.Label(root)
@@ -39,7 +40,7 @@ class MainMenu:
         GLabel_238["fg"] = "#333333"
         GLabel_238["justify"] = "center"
         GLabel_238["text"] = "Usuario:"
-        GLabel_238.place(x=70, y=130, width=104, height=32)
+        GLabel_238.place(x=80, y=130, width=104, height=32)
 
         GLabel_410 = tk.Label(root)
         ft = tkFont.Font(family='Times', size=18)
@@ -47,25 +48,26 @@ class MainMenu:
         GLabel_410["fg"] = "#333333"
         GLabel_410["justify"] = "center"
         GLabel_410["text"] = "Contraseña:"
-        GLabel_410.place(x=40, y=190, width=130, height=33)
+        GLabel_410.place(x=50, y=190, width=130, height=33)
 
         GLineEdit_742 = tk.Entry(root)
         GLineEdit_742["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times', size=10)
+        ft = tkFont.Font(family='Times', size=13)
         GLineEdit_742["font"] = ft
         GLineEdit_742["fg"] = "#333333"
         GLineEdit_742["justify"] = "center"
         GLineEdit_742["text"] = ""
-        GLineEdit_742.place(x=210, y=130, width=179, height=31)
+        GLineEdit_742.place(x=220, y=130, width=179, height=31)
 
         GLineEdit_325 = tk.Entry(root)
         GLineEdit_325["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times', size=10)
+        ft = tkFont.Font(family='Times', size=13)
         GLineEdit_325["font"] = ft
         GLineEdit_325["fg"] = "#333333"
         GLineEdit_325["justify"] = "center"
         GLineEdit_325["text"] = ""
-        GLineEdit_325.place(x=210, y=190, width=179, height=30)
+        GLineEdit_325['show'] = '*'
+        GLineEdit_325.place(x=220, y=190, width=179, height=30)
 
         GLabel_803 = tk.Label(root)
         ft = tkFont.Font(family='Times', size=10)
@@ -73,7 +75,7 @@ class MainMenu:
         GLabel_803["fg"] = "#000000"
         GLabel_803["justify"] = "center"
         GLabel_803["text"] = "Todavia no tiene una cuenta?"
-        GLabel_803.place(x=130, y=300, width=176, height=30)
+        GLabel_803.place(x=160, y=290, width=176, height=30)
 
         GButton_748 = tk.Button(root)
         GButton_748["bg"] = "#f0f0f0"
@@ -82,13 +84,22 @@ class MainMenu:
         GButton_748["fg"] = "#000000"
         GButton_748["justify"] = "center"
         GButton_748["text"] = "Crear Cuenta"
-        GButton_748.place(x=160, y=330, width=120, height=30)
+        GButton_748["relief"] = "groove"
+        GButton_748.place(x=190, y=320, width=120, height=30)
         GButton_748["command"] = self.GButton_748_command
 
-    def GButton_760_command(self):
+        GLabel_316 = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GLabel_316["font"] = ft
+        GLabel_316["fg"] = "#000000"
+        GLabel_316["justify"] = "center"
+        GLabel_316["text"] = ""
+        GLabel_316.place(x=100, y=220, width=313, height=30)
+
+    def GButton_760_command(self): #Verificar si los datos ingresados son correctos
         print("command")
 
-    def GButton_748_command(self):
+    def GButton_748_command(self): #Abrir otra ventana para lo de crear cuenta
         print("command")
 
 if __name__ == "__main__":
