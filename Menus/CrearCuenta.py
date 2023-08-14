defd65e1d67c6b494c846becd0a593bc6e06c997
emuhe <140031745+emuhe@users.noreply.github.com>
2023-08-14 13:17:49 -0500

    added a gender selector

--- Menus/CrearCuenta.py
+++ Menus/CrearCuenta.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import tkinter.font as tkFont
 from tkcalendar import DateEntry
 from datetime import datetime
@@ -28,7 +29,7 @@ class App:
         GLabel_963["font"] = ft
         GLabel_963["fg"] = "#333333"
         GLabel_963["justify"] = "left"
-        GLabel_963["text"] = "Nombre:"
+        GLabel_963["text"] = "*Nombre:"
         GLabel_963.place(x=30,y=60,width=71,height=31)
 
         GLineEdit_554=tk.Entry(self.rootCC)
@@ -45,7 +46,7 @@ class App:
         GLabel_6["font"] = ft
         GLabel_6["fg"] = "#333333"
         GLabel_6["justify"] = "left"
-        GLabel_6["text"] = "Apellido:"
+        GLabel_6["text"] = "*Apellido:"
         GLabel_6.place(x=230,y=60,width=72,height=30)
 
         GLineEdit_531=tk.Entry(self.rootCC)
@@ -62,7 +63,7 @@ class App:
         GLabel_426["font"] = ft
         GLabel_426["fg"] = "#333333"
         GLabel_426["justify"] = "left"
-        GLabel_426["text"] = "Correo:"
+        GLabel_426["text"] = "*Correo:"
         GLabel_426.place(x=30,y=110,width=72,height=35)
 
         GLineEdit_983=tk.Entry(self.rootCC)
@@ -79,13 +80,28 @@ class App:
         GLabel_842["font"] = ft
         GLabel_842["fg"] = "#333333"
         GLabel_842["justify"] = "left"
-        GLabel_842["text"] = "Fecha de Nacimiento:"
+        GLabel_842["text"] = "*Fecha de Nacimiento:"
         GLabel_842.place(x=30,y=150,width=135,height=45)
 
         GDateEntry = DateEntry(self.rootCC)
         GDateEntry['date_pattern'] = 'DD/MM/YYYY'
         GDateEntry['maxdate'] = datetime.today().date()
         GDateEntry.place (x=170,y=155,width=90,height=35)
+
+        GLabel_843=tk.Label(self.rootCC)
+        ft = tkFont.Font(family='Times',size=10)
+        GLabel_843["font"] = ft
+        GLabel_843["fg"] = "#333333"
+        GLabel_843["justify"] = "left"
+        GLabel_843["text"] = "*Genero:"
+        GLabel_843.place(x=265,y=150,width=70,height=45)
+
+        genero = tk.StringVar()
+        GComboBox = ttk.Combobox(self.rootCC)
+        GComboBox['textvariable'] = genero
+        GComboBox['values'] = ['Hombre','Mujer','Otro']
+        GComboBox['state'] = 'readonly'
+        GComboBox.place(x=335,y=155,width=75,height=35)
 
 
 
