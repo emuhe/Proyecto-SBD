05160847a2126624220cb3672f8d3be14df9ca64
emuhe <140031745+emuhe@users.noreply.github.com>
2023-08-19 11:46:17 -0500

    second selector for preferences

--- Menus/CrearCuenta.py
+++ Menus/CrearCuenta.py
@@ -32,6 +32,8 @@ class App:
         GLabel_963["text"] = "*Nombre:"
         GLabel_963.place(x=30,y=30,width=70,height=31)
 
+        self.Nombre = tk.StringVar()
+
         GLineEdit_554=tk.Entry(self.rootCC)
         GLineEdit_554["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
@@ -39,6 +41,7 @@ class App:
         GLineEdit_554["fg"] = "#333333"
         GLineEdit_554["justify"] = "center"
         GLineEdit_554["text"] = ""
+        GLineEdit_554["textvariable"] = self.Nombre
         GLineEdit_554.place(x=100,y=30,width=130,height=30)
 
         GLabel_6=tk.Label(self.rootCC)
@@ -49,6 +52,9 @@ class App:
         GLabel_6["text"] = "*Apellido:"
         GLabel_6.place(x=230,y=30,width=70,height=30)
 
+        self.Apellido = tk.StringVar()
+
+
         GLineEdit_531=tk.Entry(self.rootCC)
         GLineEdit_531["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
@@ -56,6 +62,7 @@ class App:
         GLineEdit_531["fg"] = "#333333"
         GLineEdit_531["justify"] = "center"
         GLineEdit_531["text"] = ""
+        GLineEdit_531["textvariable"] = self.Apellido
         GLineEdit_531.place(x=300,y=30,width=131,height=30)
 
         GLabel_426=tk.Label(self.rootCC)
@@ -66,6 +73,8 @@ class App:
         GLabel_426["text"] = "*Correo:"
         GLabel_426.place(x=30,y=80,width=70,height=35)
 
+        self.Correo = tk.StringVar()
+
         GLineEdit_983=tk.Entry(self.rootCC)
         GLineEdit_983["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
@@ -73,6 +82,7 @@ class App:
         GLineEdit_983["fg"] = "#333333"
         GLineEdit_983["justify"] = "center"
         GLineEdit_983["text"] = ""
+        GLineEdit_983["textvariable"] = self.Correo
         GLineEdit_983.place(x=100,y=80,width=331,height=30)
 
         GLabel_842=tk.Label(self.rootCC)
@@ -99,9 +109,10 @@ class App:
         self.genero = tk.StringVar()
         GComboBox = ttk.Combobox(self.rootCC)
         GComboBox['textvariable'] = self.genero
-        GComboBox['values'] = ['Hombre','Mujer','Otro']
+        GComboBox['values'] = ['-Seleccionar-','Hombre','Mujer','Otro']
         GComboBox['state'] = 'readonly'
-        GComboBox.place(x=335,y=125,width=75,height=35)
+        self.genero.set('-Seleccionar-')
+        GComboBox.place(x=335,y=125,width=100,height=35)
 
         GLabel_844=tk.Label(self.rootCC)
         ft = tkFont.Font(family='Times',size=10)
@@ -111,6 +122,8 @@ class App:
         GLabel_844["text"] = "*Direccion:"
         GLabel_844.place(x=30,y=170,width=75,height=30)
 
+        self.direccion = tk.StringVar()
+
         GLineEdit_984=tk.Entry(self.rootCC)
         GLineEdit_984["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
@@ -118,6 +131,7 @@ class App:
         GLineEdit_984["fg"] = "#333333"
         GLineEdit_984["justify"] = "center"
         GLineEdit_984["text"] = ""
+        GLineEdit_984['textvariable'] = self.direccion
         GLineEdit_984.place(x=100,y=170,width=331,height=30)
 
         GLabel_845 = tk.Label(self.rootCC)
@@ -145,6 +159,8 @@ class App:
         GLabel_846["text"] = "*I.D:"
         GLabel_846.place(x=240, y=215, width=75, height=30)
 
+        self.ID = tk.StringVar()
+
         GLineEdit_986 = tk.Entry(self.rootCC)
         GLineEdit_986["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times', size=10)
@@ -152,6 +168,7 @@ class App:
         GLineEdit_986["fg"] = "#333333"
         GLineEdit_986["justify"] = "center"
         GLineEdit_986["text"] = ""
+        GLineEdit_986['textvariable'] = self.ID
         GLineEdit_986.place(x=300, y=215, width=130, height=30)
 
         GLabel_846 = tk.Label(self.rootCC)
@@ -376,3 +393,43 @@ class App:
         GLabel_863["justify"] = "left"
         GLabel_863["text"] = "De Acuerdo"
         GLabel_863.place(x=345, y=435, width=105, height=30)
+
+        GButton_748 = tk.Button(self.rootCC)
+        GButton_748["bg"] = "#f0f0f0"
+        ft = tkFont.Font(family='Times', size=10)
+        GButton_748["font"] = ft
+        GButton_748["fg"] = "#000000"
+        GButton_748["justify"] = "center"
+        GButton_748["text"] = "Limpiar"
+        GButton_748["relief"] = "groove"
+        GButton_748.place(x=100, y=480, width=120, height=30)
+        GButton_748["command"] = self.LimpiarDatos
+
+        GButton_749 = tk.Button(self.rootCC)
+        GButton_749["bg"] = "#f0f0f0"
+        ft = tkFont.Font(family='Times', size=10)
+        GButton_749["font"] = ft
+        GButton_749["fg"] = "#000000"
+        GButton_749["justify"] = "center"
+        GButton_749["text"] = "Crear Cuenta"
+        GButton_749["relief"] = "groove"
+        GButton_749.place(x=260, y=480, width=120, height=30)
+        GButton_749["command"] = self.CrearCuenta
+    def CrearCuenta(self):
+        None
+    def LimpiarDatos(self):
+        print('limpio')
+        #Limpia Preferencias
+        self.Pref_4.set(2)
+        self.Pref_3.set(2)
+        self.Pref_2.set(2)
+        self.Pref_1.set(2)
+        #Limpia Entradas de texto
+        self.Nombre.set('')
+        self.Apellido.set('')
+        self.Correo.set('')
+        self.genero.set('-Seleccionar-')
+        self.direccion.set('')
+        self.ID.set('')
+
+
