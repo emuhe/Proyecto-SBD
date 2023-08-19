dcded90f7719e93df6594713043137374bc5b6a4
emuhe <140031745+emuhe@users.noreply.github.com>
2023-08-19 16:45:39 -0500

    sqlconector

---
+++ SQL_conection/conector.py
@@ -0,0 +1,7 @@
+import mysql.connector
+conection = mysql.connector.connect(
+    host="proyecto-blablacar.mysql.database.azure.com",
+    user="blablacar",
+    password="Bl@bl4c4r",
+    database="blablacar"
+)
