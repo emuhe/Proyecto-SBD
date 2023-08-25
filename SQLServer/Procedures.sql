5869f9520867cb111e417779424435d88f689b68
emuhe <140031745+emuhe@users.noreply.github.com>
2023-08-24 22:07:47 -0500

    Create Procedures.sql

---
+++ SQLServer/Procedures.sql
@@ -0,0 +1,6 @@
+DELIMITER %%
+	Create procedure BuscarViaje (in partida varchar(100))
+    BEGIN
+    SELECT punto_llegada from viaje where punto_partida = partida;
+    END %%
+CALL BuscarViaje('Guayaquil')
