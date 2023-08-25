00796582ae72cc4e52cc7d7d3cf239d247f047fc
emuhe <140031745+emuhe@users.noreply.github.com>
2023-08-25 00:12:15 -0500

    Update Procedures.sql

--- SQLServer/Procedures.sql
+++ SQLServer/Procedures.sql
@@ -5,11 +5,19 @@ DELIMITER %%
     BEGIN
     SELECT punto_llegada from viaje where punto_partida = partida;
     END %%
--- Se consiguen los datos de los viajes dependiendo del destino escogido (si no se ha escogido entonces muestra todos)
+-- Se consiguen los datos de los viajes dependiendo del destino y partida escogido (si no se ha escogido entonces muestra todos)
 DELIMITER %%
 create procedure FiltrarViaje (in partida varchar(100), in llegada varchar(100))
 BEGIN 
-IF llegada = '-Todos-' then
+IF llegada = '-Todos-' and partida = '-Todos-' then
+SELECT
+	c.nombre,c.apellido,v.punto_partida,v.punto_llegada,
+	(SELECT count(*) from reserva_asiento ra where viaje_id = v.id),
+    v.cantidad_pasajeros,v.precio_por_asiento,v.tiempo_salida,viaje_completado,
+    (select avg(val.valoracion) from valoracion val join usuario usr on val.conductor_id = usr.id
+    where usr.id = vec.conductor_id),(select vehiculo.modelo from vehiculo where vehiculo.id = vec.vehiculo_id) 
+    from viaje v join vehiculo_conductor vec on vec.id = v.vehiculo_conductor_id join usuario c on c.id = vec.conductor_id;
+elseif llegada = '-Todos-' and partida <> '-Todos-' then
 SELECT
 	c.nombre,c.apellido,v.punto_partida,v.punto_llegada,
 	(SELECT count(*) from reserva_asiento ra where viaje_id = v.id),
@@ -17,7 +25,15 @@ SELECT
     (select avg(val.valoracion) from valoracion val join usuario usr on val.conductor_id = usr.id
     where usr.id = vec.conductor_id),(select vehiculo.modelo from vehiculo where vehiculo.id = vec.vehiculo_id) 
     from viaje v join vehiculo_conductor vec on vec.id = v.vehiculo_conductor_id join usuario c on c.id = vec.conductor_id where v.punto_partida = partida;
-ELSE
+elseif llegada <> '-Todos-' and partida = '-Todos-' then
+SELECT
+	c.nombre,c.apellido,v.punto_partida,v.punto_llegada,
+	(SELECT count(*) from reserva_asiento ra where viaje_id = v.id),
+    v.cantidad_pasajeros,v.precio_por_asiento,v.tiempo_salida,viaje_completado,
+    (select avg(val.valoracion) from valoracion val join usuario usr on val.conductor_id = usr.id
+    where usr.id = vec.conductor_id),(select vehiculo.modelo from vehiculo where vehiculo.id = vec.vehiculo_id) 
+    from viaje v join vehiculo_conductor vec on vec.id = v.vehiculo_conductor_id join usuario c on c.id = vec.conductor_id where v.punto_llegada = llegada;
+else
 SELECT
 	c.nombre,c.apellido,v.punto_partida,v.punto_llegada,
 	(SELECT count(*) from reserva_asiento ra where viaje_id = v.id),
