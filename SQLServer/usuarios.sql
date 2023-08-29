5e7bec94d9d00bdc4b7f26fc30f35ed555e1bf80
emuhe <140031745+emuhe@users.noreply.github.com>
2023-08-28 23:14:58 -0500

    Create usuarios.sql

---
+++ SQLServer/usuarios.sql
@@ -0,0 +1,24 @@
+-- Crear usuarios
+CREATE USER 'Derian'@'localhost' IDENTIFIED BY '123';
+CREATE USER 'Bryan'@'localhost' IDENTIFIED BY '345';
+CREATE USER 'Kiara'@'localhost' IDENTIFIED BY '567';
+CREATE USER 'Fernando'@'localhost' IDENTIFIED BY '789';
+CREATE USER 'Carolina'@'localhost' IDENTIFIED BY '101';
+
+-- Asignar permisos al procedimiento almacenado
+GRANT EXECUTE ON PROCEDURE blablacar.procedure_name TO 'Derian'@'localhost';
+
+-- Asignar permisos a una vista
+GRANT SELECT ON blablacar.vista1 TO 'Bryan'@'localhost';
+
+-- Asignar permisos a otra vista
+GRANT SELECT ON blablacar.vista2 TO 'Kiara'@'localhost';
+
+-- Asignar permisos a un procedimiento almacenado y una vista para un usuario
+GRANT EXECUTE ON PROCEDURE blablacar.procedure_name TO 'Fernando'@'localhost';
+GRANT SELECT ON blablacar.vista1 TO 'Fernando'@'localhost';
+
+-- Asignar permisos a un procedimiento almacenado y dos vistas para otro usuario
+GRANT EXECUTE ON PROCEDURE blablacar.procedure_name TO 'Carolina'@'localhost';
+GRANT SELECT ON blablacar.vista1 TO 'Carolina'@'localhost';
+GRANT SELECT ON blablacar.vista2 TO 'Carolina'@'localhost';
