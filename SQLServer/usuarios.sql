933e38e7ade3abee72728d11d1b00e20a0a4fae2
fernan0502 <142690777+fernan0502@users.noreply.github.com>
2023-08-29 21:57:47 -0500

    Update usuarios.sql

--- SQLServer/usuarios.sql
+++ SQLServer/usuarios.sql
@@ -1,25 +1,9 @@
 -- Crear usuarios
-CREATE USER 'admin'@'localhost' IDENTIFIED BY '050';
 CREATE USER 'Derian'@'localhost' IDENTIFIED BY '123';
 CREATE USER 'Bryan'@'localhost' IDENTIFIED BY '345';
 CREATE USER 'Kiara'@'localhost' IDENTIFIED BY '567';
 CREATE USER 'Fernando'@'localhost' IDENTIFIED BY '789';
 CREATE USER 'Carolina'@'localhost' IDENTIFIED BY '101';
-
--- Asignar todos los permisos al usuario 'admin' en las tablas pero no vistas ni procedures
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.cuenta_bancaria TO 'admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.tarjeta_credito TO 'admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.usuario TO 'admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.vehiculo TO 'admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.vehiculo_conductor TO 'admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.viaje TO 'admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.reserva_asiento TO 'admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.pagos TO 'admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.valoracion TO 'admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.conversacion TO 'admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.mensaje TO 'admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON blablacar.opinion TO 'admin'@'localhost';
-FLUSH PRIVILEGES;
 
 -- Asignar permisos al procedimiento almacenado y una vista
 GRANT EXECUTE ON PROCEDURE blablacar.obtenerLlegadas TO 'Derian'@'localhost';
